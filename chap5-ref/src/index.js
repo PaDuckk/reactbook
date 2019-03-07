f70e6f8a2d950ac2a1f336d76645935b52ea718c
PaDuckk <kang10019@naver.com>
2019-03-07 20:11:51 +0900

    create styling-react

--- chap5-ref/src/index.js
+++ chap5-ref/src/index.js
@@ -16,6 +16,58 @@ class App extends Component {
     }
 }
 
+
+class Todo extends React.Component {
+    constructor(props) {
+      super(props);
+      console.log('constructor!');
+      this.state = {value: 0};
+    }
+    
+    static getDerivedStateFromProps(props, state) {
+      // props에 의존적인 state를 업데이트
+      console.log('getDerivedStateFromProps')
+      console.log('props',props)
+      console.log('state',state)
+      if (state.value !== props.value) {
+        return {
+          mirroredProp: props.value
+        }
+       }
+    // when null is returned no update is made to the state
+      return null;
+    }
+    
+    render() {
+      return (
+        <div>
+          <h1>Hello</h1>
+        </div>
+      )
+    }
+    
+    componentDidMount() {
+      console.log(this.state);
+      /*
+        Usage: All the interaction directly with the browser DOM and integrate with third-party libraries like Highcharts or D3 should be done here. E.g This method is best to draw sound wave graphs of songs.
+        
+        Where should you make the API calls?
+  The API calls should be made in componentDidMount method always.
+      */
+    }
+
+    
+  }
+  
+  
+  ReactDOM.render(
+    <Todo value={4}/>,
+    document.getElementById('root')
+  );
+  
+
+
+
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 
