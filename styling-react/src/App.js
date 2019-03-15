1d44ed24ffe0393eac0b7fbd1561254f89f76318
PaDuckk <kang10019@naver.com>
2019-03-15 15:49:38 +0900

    complete 9.3 styled-component

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import styles from'./App.scss';
-import classNames from 'classnames/bind';
-import Button from './components/Button'
+import StyledButton from './components/StyledButton';
 
-
-const cx = classNames.bind(styles);
-console.log(cx);
 class App extends Component {
   render() {
-    const isBlue = true;
-
     return (
       <div>
-        <Button>버튼</Button>
+        <StyledButton big={true}>버튼</StyledButton>
       </div>
     );
   }
