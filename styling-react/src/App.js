64d1adbe4ffeb1f4d5bde4b13b32c25d2d2f6fe5
PaDuckk <kang10019@naver.com>
2019-03-15 14:59:55 +0900

    Complete 9.2 Sass

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import styles from'./App.module.css';
+import styles from'./App.scss';
 import classNames from 'classnames/bind';
+import Button from './components/Button'
+
 
 const cx = classNames.bind(styles);
 console.log(cx);
 class App extends Component {
   render() {
-    const isBlue = false;
+    const isBlue = true;
+
     return (
-      <div className={cx('box',{
-        blue: isBlue
-      })}>
+      <div>
+        <Button>버튼</Button>
       </div>
     );
   }
