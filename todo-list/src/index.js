b9fa228990582ab71c326237edc2b4db906cc272
PaDuckk <kang10019@naver.com>
2019-03-18 21:53:34 +0900

    Complete chapter 14

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -7,7 +7,7 @@ import modules from './modules';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(modules, window.devToolsExtension && window.devToolsExtension());
+const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
