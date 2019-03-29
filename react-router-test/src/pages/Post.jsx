f57dbeb2a5b5c3dd6556e9d467a5982bfc88f985
PaDuckk <kang10019@naver.com>
2019-03-29 11:15:22 +0900

    라우트 컴포넌트에서 전달받은 props 사용법

--- react-router-test/src/pages/Post.jsx
+++ react-router-test/src/pages/Post.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-const Post = ({ match }) => {
+const Post = ({ location, match, history }) => {
+  console.log({ location, match, history });
+  const unblock = history.block("정말로떠나시겟습니까?");
+  // unblock();
   return <p>포스트 #{match.params.id}</p>;
 };
 
