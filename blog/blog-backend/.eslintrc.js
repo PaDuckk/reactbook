6b555571212cc0290c0829b6dc9795d14f069d70
PaDuckk <kang10019@naver.com>
2019-04-28 14:24:24 +0900

    initialize dev setting koa

---
+++ blog/blog-backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "no-unused-vars": 1,
+    "comma-dangle": 0,
+    "eol-last": 0,
+    "no-console": 0
+  }
+};
--- styling-react/src/components/Button/Button.js
+++ styling-react/src/components/Button/Button.js
@@ -4,12 +4,22 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const Button = ({children, ...rest}) => {
-    return (
-        <div className={cx('button')}{...rest}>
-            {children}
-        </div>
-    )
+const Button = ({
+  children,
+  ...rest
+}) => {
+  return ( <
+    div className = {
+      cx('button')
+    } {
+      ...rest
+    } > {
+      children
+    } <
+    /div>
+  )
 }
+
+
 
 export default Button;
