a699e5f617add32bc49babdeb9009c07721a04e2
PaDuckk <kang10019@naver.com>
2019-05-15 18:48:48 +0900

    add css library

---
+++ blog/blog-frontend/src/components/common/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import PropTypes from "prop-types";
+//import { Test } from './Footer.styles';
+
+const Footer = props => <div className="FooterWrapper">Test content</div>;
+
+export default Footer;
---
+++ blog/blog-frontend/src/components/common/Header/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = props => <div className="HeaderWrapper">Test content</div>;
+
+export default Header;
