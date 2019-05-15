b34fb17d2b0481f43c247cf21b0067fbecd1bfee
PaDuckk <kang10019@naver.com>
2019-05-15 22:47:33 +0900

    create PostList

---
+++ blog/blog-frontend/src/components/list/Pagination/Pagination.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './Pagination.styles';
+
+const Pagination = (props) => (
+  <div className="PaginationWrapper">
+    Test content
+  </div>
+);
+
+Pagination.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Pagination.defaultProps = {
+  // bla: 'test',
+};
+
+export default Pagination;
--- blog/blog-frontend/src/pages/ListPage.jsx
+++ blog/blog-frontend/src/pages/ListPage.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import PageTemplate from "components/common/PageTemplate";
+import ListWrapper from "components/list/ListWrapper";
+import PostList from "components/list/PostList";
 
 const ListPage = () => {
-  return <PageTemplate>List</PageTemplate>;
+  return (
+    <PageTemplate>
+      <ListWrapper>
+        <PostList />
+      </ListWrapper>
+    </PageTemplate>
+  );
 };
 
 export default ListPage;
